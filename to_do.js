0fc372826bce70b00f727ce318c1ef76be0c12a2
Henny2 <henrieke.baunack@web.de>
2023-03-12 12:30:12 -0700

    added functionality to add and save lists, and select them

--- to_do.js
+++ to_do.js
@@ -1,24 +1,74 @@
-alert('hi')
 const listContainer = document.querySelector('[data-lists]')
-let lists = ['name', 'todo']
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+// using local storage to store the information in the user's browser
+const LOCAL_STORAGE_LIST_KEY = 'task.list' // making a namespace, so that our local storage won't be overwritten
+// get list from local storage, if not exist give me an empty array
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId' //saving info on selected list in localStorage
+// get from localstorage if possible
+let selectedListId = JSON.parse(localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY))
+
+// we dynamically add lists, so adding event listeners for the onClick is more complicated
+// our solution: we add a listener to the whole container that they are in 
+listContainer.addEventListener('click', e => {
+    console.log(e)
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId
+        saveAndRender()
+    }
+})
 
 
+newListForm.addEventListener('submit', e => {
+    // stop page from refreshing because it would delete all the lists and not submit the form
+    e.preventDefault()
+    const listName = newListInput.value
+    if (listName === null || listName === '') return
+    const list = createList(listName)
+    newListInput.value = null
+    lists.push(list)
+    saveAndRender()
+
+})
+
+function createList(name) {
+    return { id: Date.now().toString(), name: name, tasks: [] }
+
+}
+
+function saveAndRender() {
+    save()
+    render()
+}
+
+// save list of lists to local storage
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, JSON.stringify(selectedListId))
+}
 
 function render() {
     clearElement(listContainer)
     lists.forEach(list => {
         const listElement = document.createElement('li')
+        listElement.dataset.listId = list.id
         listElement.classList.add('list-name')
-        listElement.innerText = list
+        listElement.innerText = list.name
+        // if the list is the selected ID, show that by adding the needed css class
+        if (list.id === selectedListId) {
+            listElement.classList.add('active-list')
+        }
         listContainer.appendChild(listElement)
     })
 }
 
 function clearElement(element) {
     while (element.firstChild) {
-        element.remmoveChild(element.firstChild)
+        element.removeChild(element.firstChild)
     }
 }
 
 render()
-alert('test')
+// stopped tutorial at 17;57
+// next up: create delete function
